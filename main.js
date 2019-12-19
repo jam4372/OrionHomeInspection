20556aabe31f5e95a447199b0b2672a818883b6e
jam4372 <31705164+jam4372@users.noreply.github.com>
2019-12-19 00:51:39 -0500

    small updates, pics and scrolling issues

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 $(document).ready(function() {
     $("#nav_services, #btn_services, #footer_services").click(function(e) {
         e.preventDefault();
-        $('html, body').animate({ scrollTop: $("#ft_services").offset().top - $("nav").height() }, 500);
+        $('html, body').animate({ scrollTop: $("#ft_services").offset().top - ($("nav").height() + 30) }, 500);
     })
 
     $("#nav_overview, #btn_overview, #footer_overview").click(function(e) {
         e.preventDefault();
-        $('html, body').animate({ scrollTop: $("#ft_overview").offset().top - $("nav").height() }, 500);
+        $('html, body').animate({ scrollTop: $("#ft_overview").offset().top - ($("nav").height() + 30) }, 500);
     })
 
     $("#nav_faq, #btn_faq, #footer_faq").click(function(e) {
         e.preventDefault();
-        $('html, body').animate({ scrollTop: $("#ft_faq").offset().top - $("nav").height() }, 500);
+        $('html, body').animate({ scrollTop: $("#ft_faq").offset().top - ($("nav").height() + 30) }, 500);
     })
 
     $("#nav_about, #footer_about").click(function(e) {
         e.preventDefault();
-        $('html, body').animate({ scrollTop: $("#ft_about").offset().top - $("nav").height() }, 500);
+        $('html, body').animate({ scrollTop: $("#ft_about").offset().top - ($("nav").height() + 30) }, 500);
     })
 
     $("#nav_contact, #jb_contacts").click(function(e) {
         e.preventDefault();
         $('html, body').animate({ scrollTop: $("#contact").offset().top }, 500);
     })
+
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
 
 })
